b79a0c88fa8a5cf6cac18be151c98fab9df3028f
Benjamin Stammen <x@benjaminstammen.com>
2022-11-16 08:33:04 -0500

    Add tests for all current account APIs
    [Problem]
    Would like to test APIs every time I build and
    avoid manually doing it.
    
    [Solution]
    Write some tests that execute against a MongoDb
    container. Remove some model objects I'm not using
    anymore.

--- src/test/kotlin/com/benjaminstammen/bfi/BfiApplicationTests.kt
+++ src/test/kotlin/com/benjaminstammen/bfi/BfiApplicationTests.kt
@@ -1,33 +1,73 @@
 package com.benjaminstammen.bfi
 
+import com.benjaminstammen.bfi.entities.Account
 import com.benjaminstammen.bfi.model.AccountMutableProperties
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.util.*
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration(initializers = [MongoInitializer::class])
+@TestMethodOrder(OrderAnnotation::class)
 class BfiApplicationTests(@Autowired val mockMvc: MockMvc) {
 
-    val mapper = ObjectMapper()
+    val mapper: ObjectMapper = ObjectMapper()
+        .registerModule(KotlinModule.Builder().build())
 
-    @Test
-    fun `test POST account`() {
-        val testAccount = AccountMutableProperties(name = "test", note = "test")
-        mockMvc.perform(post("/account")
-            .content(mapper.writeValueAsString(testAccount))
+    lateinit var persistedObject: Account
+
+    @BeforeEach
+    fun setup() {
+        val createAccountRequest = AccountMutableProperties(
+            name = "testName",
+            autoTags = listOf("testTagOne", "testTagTwo"),
+            note = "testNote"
+        )
+        val response = mockMvc.perform(post("/account")
+            .content(mapper.writeValueAsString(createAccountRequest))
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .response
+            .contentAsString
+        persistedObject = mapper.readValue(response, Account::class.java)
     }
 
+    @Test
+    fun `test account LIST`() {
+        mockMvc.perform(get("/account")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(content().json(mapper.writeValueAsString(listOf(persistedObject))))
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+    }
+
+    @Test
+    fun `test account UPDATE`() {
+        val updateRequestBody = AccountMutableProperties("newName", listOf("newTagOne", "newTagTwo"), "newNote")
+        val updatedAccount = persistedObject.mergeWithProperties(updateRequestBody)
+
+        mockMvc.perform(post("/account/${persistedObject.id}")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(updateRequestBody)))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(content().json(mapper.writeValueAsString(updatedAccount)))
+    }
 }
